6f3505140477aee900548a37ea7b0be8614d78e5
MadeByBozo <you@example.com>
2024-09-23 15:06:38 +0200

    first copper wire push

---
+++ game.js
@@ -0,0 +1,150 @@
+//Board
+let board;
+let boardWidth = 1200;
+let boardHeight = 730;
+
+let context;
+
+//Bird
+let luckyImg;
+let luckyWidth = 74;
+let luckyHeight = 40;
+let luckyX = boardWidth/8;
+let luckyY = boardHeight/2;
+let luckybait = {
+    x : luckyX,
+    y : luckyY,
+    width : luckyWidth,
+    height : luckyHeight
+}
+
+//Pipes
+let pipeArray = [];
+let pipeWidth = 20;
+let pipeHeight = 40;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImage;
+let bottomPipeImage;
+
+//Gamephysics
+let velocityX = -1; //Moving left
+let velocityY = 0; //luckybait jumping speed
+let gravity = 0.1;
+let gameOver = false;
+let score = 0;
+
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    
+    context = board.getContext("2d");
+
+
+    luckyImg = new Image();
+    luckyImg.src = "./luckybait2.png";
+    luckyImg.onload = function(){
+        context.drawImage(luckyImg, luckybait.x, luckybait.y, luckybait.width, luckybait.height)
+    }
+
+    topPipeImage = new Image();
+    bottomPipeImage = new Image();
+    topPipeImage.src = "./koppartråd.png";
+    bottomPipeImage.src = "./koppartråd.png";
+
+    requestAnimationFrame(update);
+    setInterval(placePipes, 1500); //every 1.5 seconds
+    document.addEventListener("keydown", moveBird);
+    document.addEventListener("keydown", restartGame);
+}
+
+function update(){
+    requestAnimationFrame(update);
+    if(gameOver){
+        return;
+    }
+    context.clearRect(0, 0, board.width, board.height)
+    
+    //Bird
+    velocityY += gravity;
+    luckybait.y = 100//Math.max(luckybait.y + velocityY, 0);
+    context.drawImage(luckyImg, luckybait.x, luckybait.y, luckybait.width, luckyHeight);
+    if(luckybait.y > board.height) gameOver = true;
+
+    //Pipes
+    for(let i = 0; i < pipeArray.length; i++){
+        let pipe = pipeArray[i];
+        pipe.x += velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+        if(!pipe.passed && luckybait.x > pipe.x + pipe.width) {
+            pipe.passed = true;
+            score += 0.5;
+        }
+        if(detectCollision(luckybait, pipe)){
+            gameOver = true;
+        }
+    }
+
+    context.fillStyle = "white";
+    context.font = "45px sans-serif";
+    context.fillText(score, 5, 45);
+    if(gameOver){
+        context.fillStyle = "black";
+        context.font = "25px sans-serif";
+        context.fillText("Game over", boardWidth/2 - 55, boardHeight/2);
+        context.fillText("Press ArrowUp to try again", 45, boardHeight/2 + 30);
+    }
+}
+
+function placePipes(){
+    if(gameOver){
+        return;
+    }
+    let randomPipeY = pipeY - pipeHeight/4 - Math.random() * (pipeHeight/2);
+    let openingSpace = boardHeight/4;
+    let topPipe = {
+        img : topPipeImage,
+        x : pipeX,
+        y : 300,
+        width : pipeWidth,
+        height : pipeHeight,
+        passed : false
+    }
+    // let bottomPipe = {
+    //     img : bottomPipeImage,
+    //     x : pipeX,
+    //     y : randomPipeY + pipeHeight + openingSpace,
+    //     width : pipeWidth,
+    //     height : pipeHeight,
+    //     passed : false
+    // }
+
+    pipeArray.push(topPipe)
+    // pipeArray.push(bottomPipe)
+}
+
+function moveBird(e){
+    if(e.code == "Space" || e.code == "ArrowUp"){
+        velocityY = -3
+    }
+}
+
+function detectCollision(a, b){
+    return  a.x < b.x + b.width &&
+            a.x + a.width > b.x &&
+            a.y < b.y + b.height &&
+            a.y + a.height > b.y;
+}
+
+function restartGame(e){
+    if(gameOver && e.code == "ArrowUp"){
+        gameOver = false;
+        pipeArray = []
+        luckybait.x = luckyX;
+        luckybait.y = luckyY;
+        score = 0;
+    }
+}
